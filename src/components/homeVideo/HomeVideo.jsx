c03a537ec4f2cb8b0072270a225fc4c04cfedda2
EmmanuelDarko <edarko322@gmail.com>
2023-10-11 02:11:50 +0000

    changes

--- src/components/homeVideo/HomeVideo.jsx
+++ src/components/homeVideo/HomeVideo.jsx
@@ -5,8 +5,8 @@ const HomeVideo = () => {
     <div className='homevideo__container'>
       <div className='homevideo'>
         <div className='left'>
-          <h3> Teaching Through innovation</h3>
-          <h3>Training with ease</h3>
+          <h3>Teaching</h3>
+          <h3> hrough innovation</h3>
 
           <p className='desc'>
             Equip your students for the digital age. Our
